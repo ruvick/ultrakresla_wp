f3703cc5bd1d94092ef0b7e5ddad6ab35977e68e
Ruvick <rudikov.web@ya.ru>
2021-05-17 19:06:51 +0300

    Треугольные блоки в Полезные материалы

--- template-parts/material-elem.php
+++ template-parts/material-elem.php
@@ -1,5 +1,5 @@
-<a href = "<?php the_permalink();?>" class = "cat_elem">
+<a href = "<?php the_permalink();?>" class = "cat_elem color-t">
     <img src = "<?php the_post_thumbnail_url( "large" ); ?>" alt = "<?the_title()?>" title = "<?the_title()?>" />
-    <div class = "nuar_blk"></div>
+    <div class = "nuar_blk color-t"></div>
     <p class = "title"><?the_title()?></p>
 </a>

076cb421a0e002ad6a22f8dfcb42d94a6c1306af
Ruvick <rudikov.web@ya.ru>
2021-05-17 23:50:58 +0300

    Угловой цветной блок в Галерее работ

--- category-galereya-nashih-rabot.php
+++ category-galereya-nashih-rabot.php
@@ -18,37 +18,25 @@
 			<div class="galery-block-cat galery-block">
 					<h1><?php single_cat_title( '', true );?></h1>
 
-					<?php
-  					$args = array( 'numberposts' => -1, 'order' => 'ASC', 'category' => 21 );
-  					$myposts = get_posts( $args );
-  						foreach( $myposts as $post ){
-    					setup_postdata($post);
-    				?>
-							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?>(<?php echo carbon_get_post_meta(get_the_ID(),"number_img"); ?>)</a></h3>
-							<a href="<?php the_permalink(); ?>" class="galery-block__galery-row">
-						<?
-							$galw = carbon_get_the_post_meta('galery_works');
-								if($galw) {
-							$galwIndex = 0;
-							foreach($galw as $galw_item) { 
+					<div class="galery-block__galery-row">
+						<?php global $post; 
+							$args = array( 'posts_per_page' => -1, 'order' => 'ASC', 'category' => 21 );
+							$myposts = get_posts( $args );
+								foreach( $myposts as $post ){ setup_postdata($post);
+									$galw = carbon_get_the_post_meta('galery_works');
+								foreach($galw as $galw_item);
 						?>
-							<div class="galery-block__galery-img">
-								<img
-									id = "pict-<? echo empty($galw_item['galery_works_img_sku'])?$galwIndex:$galw_item['galery_works_img_sku']; ?>" 
-									alt = "<? echo $galw_item['galery_works_img_alt']; ?>"
-									title = "<? echo $galw_item['galery_works_img_alt']; ?>"
-									src = "<?php echo wp_get_attachment_image_src($galw_item['galery_works_img'], 'full')[0];?>" />
-							</div>
-						<?
-								$galwIndex++;
-								}
-							}
-						?>
+							<a href="<?php the_permalink(); ?>" class="galery-block__galery-img">
+								<img src = "<?php echo wp_get_attachment_image_src($galw_item['galery_works_img'], 'full')[0];?>" />
+								<div class="galery-color-block color-t-left">
+									<p><?php the_title(); ?>(<?php echo carbon_get_post_meta(get_the_ID(),"number_img"); ?>)</p>
+								</div>
 							</a>
-    			<?php 
- 						}
-  					wp_reset_postdata();
-  				?>
+						<?php
+							}
+							wp_reset_postdata();
+						?>
+					</div>
 			</div>
 
 			<?php if ( function_exists( 'wp_corenavi' ) ) wp_corenavi(); ?>
@@ -56,13 +44,13 @@
 		</div>
 	</section>
 
-	<section class="category-description"> 
+	<!-- <section class="category-description"> 
 		<div class="container">
 			<div class="category-description__text text_blk">
 					<?php echo category_description(); ?>
 			</div>
 		</div>
-	</section>
+	</section> -->
 
 </main>
 

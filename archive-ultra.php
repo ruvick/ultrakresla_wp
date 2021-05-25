106f3f293a5677391ba16928b1e6fe696f7f9a1b
Ruvick <rudikov.web@ya.ru>
2021-05-25 13:56:13 +0300

    Изменил таксономию archive-ultra

--- archive-ultra.php
+++ archive-ultra.php
@@ -17,18 +17,34 @@
 
 			<h1><?php single_cat_title( '', true );?></h1>
 
-			<div class="prod-card d-flex">
+			<div class="archive-prod-card galery-block__galery-row prod-card">
+			<?php
+				$terms = get_terms( 'ultracat' );
 
-				<?php
-				while(have_posts()):
-					the_post();
-					get_template_part('template-parts/product-elem');  
-				endwhile;
-				?>
+					if( $terms && ! is_wp_error($terms) ){
+					
+					foreach( $terms as $term ) {
 
+						$term_id = $term->term_taxonomy_id;
+						// получим ID картинки из метаполя термина
+						$image_id = get_term_meta( $term_id, '_thumbnail_id', 1 );
+						// ссылка на полный размер картинки по ID вложения
+						$image_url = wp_get_attachment_image_url( $image_id, 'full' );
+
+						echo "<a href='". get_term_link( $term->term_id, $term->taxonomy ) ."' class='galery-block__galery-img'>
+						<img src = '" . $image_url . "' /> 
+						<div class='galery-color-block color-t-left'>
+							<p class = 'title'>". $term->name ."</p>
+						</div>
+					</a>";
+					
+					}
+					
+					}
+			?>
 			</div>
 
-			<?php if ( function_exists( 'wp_corenavi' ) ) wp_corenavi(); ?>
+			<!-- <?php if ( function_exists( 'wp_corenavi' ) ) wp_corenavi(); ?> -->
 
 		</div>
 	</section>
